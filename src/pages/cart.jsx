42761adfb26721eed3e54b596b10eff5bcf29abc
bnjmn <bmcode19@gmail.com>
2023-10-03 16:11:40 +0100

    #added-functionality to load more food items

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,45 +1,51 @@
-import React, { useContext } from 'react'
-import { Bar } from '../components/navbar'
-import { ShopContext } from '../context/shopContext'
-import { FOOD } from '../Food'
-import { CartItem } from './Cartitem'
-import { useNavigate } from 'react-router-dom'
-
-
+import React, { useContext } from 'react';
+import { Bar } from '../components/navbar';
+import { ShopContext } from '../context/shopContext';
+import { FOOD } from '../Food';
+import { CartItem } from './Cartitem';
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
-  const { cartItems, getTotalAmount } = useContext(ShopContext)
-  const totalAmount = getTotalAmount()
+  const { cartItems, getTotalAmount, updateCartItem } = useContext(ShopContext);
+  const totalAmount = getTotalAmount();
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
+  const handleCheckout = () => {
+    // Clear the cart by setting the quantity of all items to 0
+    for (const item in cartItems) {
+      updateCartItem(0, item);
+    }
+    // You can also clear the cart from local storage if needed
+    localStorage.removeItem('cart');
+    // Navigate to the checkout page
+    navigate('/checkout');
+  };
+
   return (
     <div>
       <Bar />
-
       <div className="cart-box">
         {FOOD.map((product) => {
           if (cartItems[product.id] !== 0) {
-            return <CartItem data={product} />
+            return <CartItem key={product.id} data={product} />;
           }
         })}
       </div>
       <div className='total-amount'>
-      {totalAmount > 0 ? (
-        <>
-          <div className="total">
-            <h1>Total: <span className="amount"> ${totalAmount.toFixed(2)}</span></h1>
-          </div>
-          <button onClick={() => navigate('/cart')} className='continue'>Continue Shopping</button>
-          <button className="checkout hidden"  onClick={() => navigate('/checkout')}>checkout</button>
-        </>
-      ) : (
-        <h2>Your cart is Empty</h2>
-      )}
+        {totalAmount > 0 ? (
+          <>
+            <div className="total">
+              <h1>Total: <span className="amount"> ${totalAmount.toFixed(2)}</span></h1>
+            </div>
+            <button onClick={() => navigate('/menu')} className='continue'>Continue Shopping</button>
+            <button className="checkout hidden" onClick={handleCheckout}>Checkout</button>
+          </>
+        ) : (
+          <h2>Your cart is Empty</h2>
+        )}
       </div>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-
-
-export default Cart
+export default Cart;
--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
-// import Navbar from '../components/navbar'
-import Navbar, { Bar } from '../components/navbar'
-import { FOOD } from '../Food'
-import Product from './Product'
-
+import React, { useState } from 'react';
+import Navbar from '../components/navbar';
+import { FOOD } from '../Food';
+import Product from './Product';
 
 const Menu = () => {
-    return (
-        <>
-            <Navbar />
+  const [visibleProducts, setVisibleProducts] = useState(6); // Number of products to initially display
 
-            <div className='sushi-foods'>
-                <div className='foods-cards'>
+  const loadMoreProducts = () => {
+    // Increase the number of products to display
+    setVisibleProducts((prevVisibleProducts) => prevVisibleProducts + 6); // Increase by 6 or any desired number
+  };
 
-                    {FOOD.map((food) => (
-                        <Product data={food} />
-                    ))}
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
 
-export default Menu
+      <div className='sushi-foods'>
+        <div className='foods-cards'>
+          {FOOD.slice(0, visibleProducts).map((food) => (
+            <Product key={food.id} data={food} />
+          ))}
+        </div>
 
+        {visibleProducts < FOOD.length && (
+          <button onClick={loadMoreProducts} className='load-more-button'>
+            Load More
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Menu;
